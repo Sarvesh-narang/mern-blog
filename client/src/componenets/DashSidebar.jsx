88ce1837500c12c3d252f6955ba866320d35377d
Sarvesh Narang <sarvesh13narang@gmail.com>
2024-05-17 04:15:51 +0530

    complete user image upload functionality

--- client/src/componenets/DashSidebar.jsx
+++ client/src/componenets/DashSidebar.jsx
@@ -17,15 +17,16 @@ function DashSidebar() {
       <Sidebar className="w-full md-w-56">
         <Sidebar.Items>
             <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab=profile'>
             <Sidebar.Item active={tab === 'profile'}
              icon={HiUser}
              label ={"User"} 
              labelColor='dark'
+             as='div'
              >
+          <Link to='/dashboard?tab=profile'>
                 Profile
+          </Link>
             </Sidebar.Item>
-            </Link>
             <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer'>
                 Sign out
             </Sidebar.Item>
